87cea8566a07a38ca144be92a9229db77b1d5e29
dontTakeMyName <9.2ShinHayek@gmail.com>
2022-04-24 18:24:28 +0700

    insert database

--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -11,6 +11,11 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return self.name
 class Product(models.Model):
     CATEGORY = (
             ('Indoor', 'Indoor'),
@@ -20,17 +25,20 @@ class Product(models.Model):
     name = models.CharField(max_length=50, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=50, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=50, null=True)
+    description = models.CharField(max_length=50, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
 
+    def __str__(self):  
+        return self.name
 class Order(models.Model):
     STATUS = (
             ('Periding', 'Pending'),
             ('Out for delivery', 'Out for delivery'),
             ('Delivered', 'Delivered'),
     )
-    # customer = 
-    # product = 
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
 
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=50, null=True, choices=STATUS)
