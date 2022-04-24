42337db37e3fdcf60241fb37638f2cb93ed4bcdc
dontTakeMyName <9.2ShinHayek@gmail.com>
2022-04-24 23:15:45 +0700

    inline formset

--- crm1/accounts/urls.py
+++ crm1/accounts/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('products/', views.products, name="products"),
     path('customer/<str:pk>/', views.customer, name="customer"),
 
-    path('create_order/', view=views.createOrder, name="create_order"),
+    path('create_order/<str:pk>/', view=views.createOrder, name="create_order"),
     path('update_order/<str:pk>/', view=views.updateOrder, name="update_order"),
     path('delete_order/<str:pk>/', view=views.deleteOrder, name="delete_order"),
 
